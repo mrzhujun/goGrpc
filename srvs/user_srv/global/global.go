c4c6a1ef5857b896af0457c75b8b65a650d708c7
zhujun <450453892@qq.com>
2021-09-07 21:35:36 +0800

    protobuf + user模型

---
+++ srvs/user_srv/global/global.go
@@ -0,0 +1,40 @@
+package global
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	Db *gorm.DB
+)
+
+func init() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Info, // 日志级别
+			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,        // 禁用彩色打印
+		},
+	)
+
+	// 全局模式
+	var err error
+	dsn := "root:root@tcp(127.0.0.1:3306)/user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
--- srvs/user_srv/hanlder/user.go
+++ srvs/user_srv/hanlder/user.go
@@ -1,1 +1,61 @@
 package hanlder
+
+import (
+	"context"
+	"gorm.io/gorm"
+	"mxshop_servs/srvs/user_srv/global"
+	"mxshop_servs/srvs/user_srv/model"
+	"mxshop_servs/srvs/user_srv/proto"
+)
+
+type UserServer struct {
+}
+
+func ModelToResponse(user model.User) proto.UserInfoResponse {
+	userInfoRsp := proto.UserInfoResponse{
+		Id:       user.ID,
+		Password: user.Password,
+		NickName: user.NickName,
+		Gender:   user.Gender,
+		Role:     uint32(user.Role),
+	}
+
+	if user.Birthday != nil {
+		userInfoRsp.BirthDay = uint64(user.Birthday.Unix())
+	}
+	return userInfoRsp
+}
+
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error) {
+	//获取用户列表
+	var users []model.User
+	result := global.Db.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	rsp := &proto.UserListResponse{}
+	rsp.Total = int32(result.RowsAffected)
+
+	global.Db.Scopes(Paginate(int(req.Pn), int(req.PSize))).Find(&users)
+	for _, user := range users {
+		userInfoRsp := ModelToResponse(user)
+		rsp.Data = append(rsp.Data, &userInfoRsp)
+	}
+	return rsp, nil
+}
